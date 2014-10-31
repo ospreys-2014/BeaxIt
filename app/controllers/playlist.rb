f2f63b564c7c320ae2625f24284e0d31f264ef5f
Baron Willeford <baron.willeford@gmail.com>
2014-10-31 16:19:59 -0400

    worked on playlists

--- app/controllers/playlist.rb
+++ app/controllers/playlist.rb
@@ -4,4 +4,12 @@ get '/playlist/:id' do |id|
   erb :'playlist/list_tracks', locals: {playlist: playlist}
 end
 
+get '/add_song' do
+  erb :'/playlist/add_song'
+end
 
+put '/add_song' do
+  Playlist.find_by(name = :playlist).tracks << Track.create(params[:song])
+
+  redirect '/playlist/1'
+end
