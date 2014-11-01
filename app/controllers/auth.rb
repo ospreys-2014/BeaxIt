d3cf40af342e8fcc3a2532ce5a88abe8f62f3a28
xiranou <xiranou@gmail.com>
2014-11-01 14:45:56 -0400

    initial session

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,4 +1,11 @@
 get '/' do
-  erb :welcome
+  if session[:user_id]
+    @playlists = Playlist.where(user_id: session[:user_id])
+    @user = User.find(session[:user_id])
+
+    erb :welcome, locals:{playlists: @playlists, user: @user}
+  else
+    erb :welcome
+  end
 end
 
