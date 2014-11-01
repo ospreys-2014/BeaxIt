87da412746f71b6af22eaa1aeb78e77d92773f9a
xiranou <xiranou@gmail.com>
2014-11-01 11:12:35 -0400

    comment out unuse seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
-track_dir = Dir["../**/*.mp3"]
-track_dir.each do |dir|
-  Track.create(title: dir.match(/(\w+)\.mp3/)[1], link: File.expand_path(dir))
-end
+# require 'itunes-search-api'
+# bloom = ITunesSearchAPI.search(term: 'Bloom', entity: 'song', :media => "music")
+# p bloom[10]
+# bloom.each do |track|
+#   # p track['trackName']
+# end
