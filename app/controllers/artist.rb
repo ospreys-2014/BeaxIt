23445f39f251976089146e1db9e3e54cfdf16434
xiranou <xiranou@gmail.com>
2014-11-01 11:49:46 -0400

    initial artists function

---
+++ app/controllers/artist.rb
@@ -0,0 +1,11 @@
+get '/artists' do
+  @artists = Artist.all
+
+  erb :"artists/all", locals:{artists: @artists}
+end
+
+get '/artist/:id' do |id|
+  @artist = Artist.find(id)
+
+  erb :"artists/single", locals:{artist: @artist}
+end
