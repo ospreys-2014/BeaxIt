f2a249b58cac6f48354eac0d155263a536bf62e2
xiranou <xiranou@gmail.com>
2014-11-01 11:18:03 -0400

    remove unuse code form Track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,8 +2,4 @@ class Track < ActiveRecord::Base
   belongs_to :album
   belongs_to :artist
   belongs_to :playlist
-
-  def correct_link
-    self.link.sub(/\/Users\/xiranou/, 'http://localhost:9393')
-  end
 end
