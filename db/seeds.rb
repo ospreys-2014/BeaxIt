bbee6201e30d99cc52659d59667c1961a14492b5
xiranou <xiranou@gmail.com>
2014-11-02 11:23:02 -0500

    update seed function with mp3info

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,12 @@
-require 'mp3info'
-
 dir = Dir["public/tracks/**/*.mp3"]
 dir.each do |track|
   Mp3Info.open(track) do |info|
-    p info.tag.title
+    track = Track.new(title: info.tag.title)
+    artist = Artist.find_or_create_by(name: info.tag.name)
+    album = Album.find_or_create_by(title: info.tag.album)
+    track.artist, track.album = artist, album
+    album.artist = artist
+    track.save
+    album.save
   end
 end
-
-# Track.all.each do |track|
-#   track.update(link: "tracks/#{track.title}")
-#   track.save
-# end
-
-# Track.all.each do |track|
-#   track.artist = Artist.first
-#   track.save
-# end
-
-# Track.all.each do |track|
-#   track.album = Album.first
-#   track.save
-# end
-
-# Track.all.each do |track|
-#   track.playlist = Playlist.first
-#   track.save
-# end
-
-# Playlist.all.each do |playlist|
-#   playlist.user = User.first
-#   playlist.save
-# end
