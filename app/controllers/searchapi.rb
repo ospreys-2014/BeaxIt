e06ac003104ea3fb9de756a1f33733e425a86fd5
xiranou <xiranou@gmail.com>
2014-11-02 18:23:43 -0500

    update api search in controller

--- app/controllers/searchapi.rb
+++ app/controllers/searchapi.rb
@@ -3,10 +3,9 @@ get '/searchapi' do
 end
 
 post '/searchapi' do
-p params[:artist]
 itunes_library = ITunesSearchAPI.search(term: "#{params[:artist]}", entity: 'song', :media => "music")
 	itunes_library.each do |info|
-	  track = Track.create(title: info['trackName'], link: info['previewUrl'])
+	  track = Track.find_or_create_by(title: info['trackName'], link: info['previewUrl'])
 	  album = Album.find_or_create_by(title: info['collectionName'])
 	  artist = Artist.find_or_create_by(name: info['artistName'])
 	  track.artist, track.album = artist, album
