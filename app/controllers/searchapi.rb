8b0c73ba051370c203c7901ccc35c5e49752bd72
xiranou <xiranou@gmail.com>
2014-11-02 18:34:22 -0500

    add artist now redirects to aritst/:id

--- app/controllers/searchapi.rb
+++ app/controllers/searchapi.rb
@@ -7,13 +7,14 @@ itunes_library = ITunesSearchAPI.search(term: "#{params[:artist]}", entity: 'son
 	itunes_library.each do |info|
 	  track = Track.find_or_create_by(title: info['trackName'], link: info['previewUrl'])
 	  album = Album.find_or_create_by(title: info['collectionName'])
-	  artist = Artist.find_or_create_by(name: info['artistName'])
-	  track.artist, track.album = artist, album
-	  artist.albums << album
+	  @artist = Artist.find_or_create_by(name: info['artistName'])
+	  track.artist, track.album = @artist, album
+	  @artist.albums << album
 	  album.tracks << track
 	  track.save
 	  album.save
-	  artist.save
+	  @artist.save
 	end
-	redirect'/'
+
+	redirect "/artist/#{@artist.id}"
 end
