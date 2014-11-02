4b369043c874c7046a57dfd5658ecad06682a07c
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-02 00:26:47 -0400

    added js login and sign up toggles

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+  $(document).on("click", "#signup", function(event){
+    event.preventDefault();
+    $("#showsignup").toggle();
+  })
+  $(document).on("click", "#login", function(event){
+    event.preventDefault();
+    $("#showlogin").toggle();
+  })
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
