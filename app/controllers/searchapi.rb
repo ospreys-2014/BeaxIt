05bf88459471ebfd3d5ddca050941652bebe80ed
Ali Yeysides <ali.yeysides@gmail.com>
2014-11-02 19:30:59 -0500

    add artwork, add itunes link

--- app/controllers/searchapi.rb
+++ app/controllers/searchapi.rb
@@ -6,7 +6,7 @@ post '/searchapi' do
 itunes_library = ITunesSearchAPI.search(term: "#{params[:artist]}", entity: 'song', :media => "music")
 	itunes_library.each do |info|
 	  track = Track.find_or_create_by(title: info['trackName'], link: info['previewUrl'])
-	  album = Album.find_or_create_by(title: info['collectionName'])
+	  album = Album.find_or_create_by(title: info['collectionName'], artwork_url: info['artworkUrl100'], preview: info['collectionViewUrl'])
 	  artist = Artist.find_or_create_by(name: info['artistName'])
 	  track.artist, track.album = artist, album
 	  artist.albums << album
