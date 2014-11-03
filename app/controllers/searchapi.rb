ea91f0a76a946868819746be3219faf76916f6c3
xiranou <xiranou@gmail.com>
2014-11-02 20:57:14 -0500

    add error for no artist found

--- app/controllers/searchapi.rb
+++ app/controllers/searchapi.rb
@@ -3,17 +3,20 @@ get '/searchapi' do
 end
 
 post '/searchapi' do
-itunes_library = ITunesSearchAPI.search(term: "#{params[:artist]}", entity: 'song', :media => "music")
-	itunes_library.each do |info|
-	  track = Track.find_or_create_by(title: info['trackName'], link: info['previewUrl'])
-	  album = Album.find_or_create_by(title: info['collectionName'])
-	  artist = Artist.find_or_create_by(name: info['artistName'])
-	  track.artist, track.album = artist, album
-	  artist.albums << album
-	  album.tracks << track
-	  track.save
-	  album.save
-	  artist.save
-	end
+
+# itunes_library = ITunesSearchAPI.search(term: "#{params[:artist]}", entity: 'song', :media => "music")
+# 	itunes_library.each do |info|
+# 	  artist = Artist.find_or_create_by(name: info['artistName'])
+# 	  track = Track.find_or_create_by(title: info['trackName'], link: info['previewUrl'])
+# 	  album = Album.find_or_create_by(title: info['collectionName'])
+# 	  track.artist, track.album = artist, album
+# 	  artist.albums << album
+# 	  album.tracks << track
+# 		track.save
+# 	  album.save
+# 	  artist.save
+# 	end
+	search_by_artist(params[:artist])
+	# session[:artist_not_found] = "Artist Not Found!" if @artist.nil?
 	redirect'/'
 end
