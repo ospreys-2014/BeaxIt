d981a50391c28f9aa15ab6050a79e1c9a8d34026
xiranou <xiranou@gmail.com>
2014-11-02 22:07:51 -0500

    update searchapi

--- app/controllers/searchapi.rb
+++ app/controllers/searchapi.rb
@@ -3,10 +3,10 @@ get '/searchapi' do
 end
 
 post '/searchapi' do
-	id = search_by_artist(params[:artist])
+	search_by_artist(params[:artist])
 	if session[:no_artist_found]
 		redirect "/"
 	else
-		redirect "/artist/#{id}"
+		redirect "/tracks"
 	end
 end
